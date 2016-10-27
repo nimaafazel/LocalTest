6f786ff3c80c922323da5c1a56f8e989e5c40fbc
Nima Afazel <NAfazel@bh.lacounty.gov>
2016-10-27 11:20:35 -0700

    first commit

---
+++ LocalGit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LocalGit.MvcApplication" Language="C#" %>
---
+++ LocalGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LocalGit.Startup))]
+namespace LocalGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
