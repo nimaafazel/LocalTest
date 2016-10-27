3836bcbd54c184faa21344af3e19b698494dc6f9
Nima Afazel <NAfazel@bh.lacounty.gov>
2016-10-27 11:47:24 -0700

    add student model

---
+++ LocalGit/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocalGit.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public DateTime dob { get; set; }
+        public int grade { get; set; }
+    }
+}
