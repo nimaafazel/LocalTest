f3db236baae27d826fde76b17198b8b3a46229c1
Nima Afazel <NAfazel@bh.lacounty.gov>
2016-10-27 13:23:28 -0700

    add StudentContext class

---
+++ LocalGit/Models/StudentContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace LocalGit.Models
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> students { get; set; }
+    }
+}
